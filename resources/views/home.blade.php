12761ba82ac26610b60a89f1a9d4602c01c11433
Ivan <88528389+IvanVasil-ev@users.noreply.github.com>
2022-02-06 23:33:39 +0300

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo List</title>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css" rel="stylesheet" />
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.js"></script>
+</head>
+
+<body class="bg-dark">
+    <div class="container w-25 mt-5">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <h3>To-do List</h3>
+                <form action="{{ route('store') }}" method="POST" autocomplete="off">
+                    @csrf
+                    <div class="input-group">
+                        <input type="text" name="content" class="form-control" placeholder="Enter your task here...">
+                        <button type="submit" class="btn btn-dark btn-sm px-4"><i class="fas fa-plus"></i></button>
+                    </div>
+                </form>
+                {{-- if tasks count --}}
+                @if (count($todolists))
+                    <ul class="list-group list-group-flush mt-3">
+                        @foreach($todolists as $todolist)
+                            <li class="list-group-item">
+                                <form action="{{ route('destroy', $todolist->id) }}" method="POST">
+                                    {{ $todolist->content }}
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn btn-link btn-sm float-end">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </form>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+            </div>
+        </div>
+    </div>
+</body>
+</html>
